121741fb01c459e4dbfa4565cc7ec481e32db5b2
Leon <70642305+LeonBoussen@users.noreply.github.com>
2020-10-29 09:23:19 +0100

    Add files via upload

---
+++ 01-Turtle/hidde.py
@@ -0,0 +1,17 @@
+import turtle
+from random import randrange
+
+
+for x in range(99999999999999999999999999999999999):
+    num = (randrange(100))
+
+    turtle.forward(num)
+    turtle.right(num)
+    turtle.forward(num)
+    turtle.right(num)
+    turtle.forward(num)
+    turtle.right(num)
+    turtle.forward(num)
+    turtle.right(num)
+    
+turtle.done()
