6f1a76f7ab21ae95aaccd32e7244f0ec98a42d17
Leon <70642305+LeonBoussen@users.noreply.github.com>
2020-10-29 09:43:13 +0100

    Create toon_lijst.py

---
+++ 02FileFolders/toon_lijst.py
@@ -0,0 +1,20 @@
+import os
+import time
+
+huidige_map = os.getcwd()
+
+alle_bestanden = os.scandir(huidige_map)
+
+print("Inhoudsopgave van de map: " + huidige_map)
+for bestand in alle_bestanden:    
+    if bestand.is_file():
+        # Dit wordt uitgevoerd als dit een normale file is
+        print(bestand.name + " (BESTAND)")
+    elif bestand.is_dir():
+        # Dit wordt uitgevoerd als dit een dir(ectory) is
+        print(bestand.name + " (MAP)")
+    else:
+        # Dit wordt uitgeveord als het geen file en geen dir is
+        print(bestand.name + " (ONBEKEND TYPE")
+
+time.sleep(10)
